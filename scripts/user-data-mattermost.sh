04262a05c0013257e43f134c3770094b43be6d6e
chloe-t <chloe.trouilh@gmail.com>
2023-03-13 22:06:30 +0100

    update user-data script

--- scripts/user-data-mattermost.sh
+++ scripts/user-data-mattermost.sh
@@ -10,6 +10,19 @@ systemctl start docker
 
 # Install Docker compose
 apt-get update -y
+apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update -y
 apt-get install docker-compose-plugin -y
 
 # Install Mattermost

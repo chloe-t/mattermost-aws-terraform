48b8183641266dfc5c3e757c31a030aa42b4b801
chloe-t <chloe.trouilh@gmail.com>
2023-03-13 08:42:44 +0100

    Update cidr block

--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -31,7 +31,7 @@ variable "create_vpc" {
 variable "cidr" {
   description = "The CIDR block for the VPC. Default value is a valid CIDR, but not acceptable by AWS and should be overridden"
   type        = string
-  default     = "0.0.0.0/0"
+  default     = "10.10.10.0/24"
 }
 
 variable "instance_tenancy" {
